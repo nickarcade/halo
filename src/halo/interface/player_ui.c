a19923a88f3d2574e69d113d923e50a087b09732
Stian Eklund <stian.eklund@gmail.com>
2026-04-12 19:08:46 +0200

    Implement input_frame_begin/end, player_ui_dispose, game_state_dispose_from_old_map
    Add input_frame_begin (polls device states, sets frame tick) and
    input_frame_end (clears frame tick) for input_xbox.obj. Add
    player_ui_dispose (zeroes UI globals) and
    game_state_dispose_from_old_map
    (empty stub matching original).
    
    Add kb.json entries: input_get_device_states, input_frame_tick,
    player_ui_globals, LIBCMT:strncpy.

--- src/halo/interface/player_ui.c
+++ src/halo/interface/player_ui.c
@@ -1,6 +1,12 @@
-__int16 player_ui_get_single_player_local_player_controller(
-  __int16 local_player_index)
+void player_ui_dispose(void)
 {
-  assert_halt_msg(local_player_index >= 0 && local_player_index < 4, "invalid local player index");
+  csmemset(player_ui_globals, 0, sizeof(player_ui_globals));
+}
+
+__int16
+player_ui_get_single_player_local_player_controller(__int16 local_player_index)
+{
+  assert_halt_msg(local_player_index >= 0 && local_player_index < 4,
+                  "invalid local player index");
   return word_46BFC4[(__int16)local_player_index];
 }
