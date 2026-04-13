a952e3043237e0c3680a9886e16cd1e7afda130b
Stian Eklund <stian.eklund@gmail.com>
2026-04-13 14:14:52 +0200

    Implement hud_update + hud_get_item_string + hud_autosave,
    completing hud.obj (4/4)
    
    hud_update: calls 4 HUD subsystem updates, then for each local player
    with a valid handle, passes the player datum to the HUD renderer.
    
    hud_get_item_string: looks up a wchar_t string from the HUD's unicode
    string list tag ('ustr'), returning a default string if the tag or
    index is invalid.
    
    hud_autosave: displays the autosave/revert message to all active local
    players using hud_get_item_string, optionally triggering a sound.

--- src/halo/interface/hud.c
+++ src/halo/interface/hud.c
@@ -1,3 +1,38 @@
+void hud_update(void)
+{
+  int i;
+  int player_handle;
+  void *player;
+
+  ((void (*)(void))0xda980)();
+  ((void (*)(void))0xd7d10)();
+  ((void (*)(void))0xd7080)();
+  ((void (*)(void))0xd51b0)();
+  if (game_engine_force_single_screen()) {
+    for (i = 0; (int16_t)i < 4; i++) {
+      player_handle = local_player_get_player_index(i);
+      if (player_handle != -1) {
+        player = datum_get(player_data, player_handle);
+        ((void (*)(void *, int))0xd7560)(player, 0);
+      }
+    }
+  }
+}
+
+wchar_t *hud_get_item_string(int index)
+{
+  int tag_handle;
+  int *tag;
+
+  tag_handle = *(int *)((char *)hud_globals + 0xa0);
+  if (tag_handle != -1) {
+    tag = (int *)tag_get(0x75737472, tag_handle);
+    if (tag != NULL && index >= 0 && index < *tag)
+      return (wchar_t *)((int (*)(int, int))0x19d420)(tag_handle, index);
+  }
+  return *(wchar_t **)0x2f66bc;
+}
+
 void hud_load(bool a1)
 {
   __int16 v1;
@@ -10,5 +45,31 @@ void hud_load(bool a1)
   scripted_hud_messages_clear();
   if (v1 != -1) {
     hud_print_message(local_player_get_next(-1), hud_get_item_string(v1));
+  }
+}
+
+void hud_autosave(int16_t param)
+{
+  int16_t string_index;
+  int i;
+  int player_handle;
+  wchar_t *message;
+
+  if (param == 0)
+    string_index = *(int16_t *)((char *)hud_globals + 0x3de);
+  else
+    string_index = *(int16_t *)((char *)hud_globals + 0x3dc);
+  ((void (*)(void))0xd5120)();
+  if (param != 0 && *(int *)((char *)hud_globals + 0x3ec) != -1)
+    ((void (*)(int, int))0x1c7480)(*(int *)((char *)hud_globals + 0x3ec),
+                                   0x3f800000);
+  if (string_index != -1) {
+    for (i = 0; (int16_t)i < 4; i++) {
+      player_handle = local_player_get_player_index(i);
+      if (player_handle != -1) {
+        message = hud_get_item_string(string_index);
+        ((void (*)(int, wchar_t *))0xd51c0)(i, message);
+      }
+    }
   }
 }
