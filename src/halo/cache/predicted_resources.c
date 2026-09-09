79bb3dfc760f4290bc7b9a5b54850b2979b72866
Stian Eklund <stian.eklund@gmail.com>
2026-09-09 09:53:59 +0200

    predicted_resources: improve precache and helper scores (68.6% -> 98.0%, 87.0% -> 93.1% VC71)

--- src/halo/cache/predicted_resources.c
+++ src/halo/cache/predicted_resources.c
@@ -31,19 +31,19 @@ int FUN_001bdd70(void)
  * each to be loaded into the sound cache. */
 void FUN_001bdd80(int tag_index)
 {
-  char *sound_data = (char *)tag_get(0x736e6421, tag_index);
-  int *pitch_ranges = (int *)(sound_data + 0x98);
+  int *pitch_ranges;
+  char *pitch_range;
   int16_t i;
+  int16_t j;
+
+  pitch_ranges = (int *)((char *)tag_get(0x736e6421, tag_index) + 0x98);
 
   for (i = 0; i < *pitch_ranges; i++) {
-    char *pitch_range =
-      (char *)tag_block_get_element(pitch_ranges, (int)i, 0x48);
-    int16_t j;
+    pitch_range = (char *)tag_block_get_element(pitch_ranges, (int)i, 0x48);
 
     for (j = 0; j < *(int16_t *)(pitch_range + 0x2c); j++) {
-      void *permutation =
-        tag_block_get_element((void *)(pitch_range + 0x3c), (int)j, 0x7c);
-      sound_cache_request_sound(permutation, 0, 1, 0);
+      sound_cache_request_sound(
+        tag_block_get_element((void *)(pitch_range + 0x3c), (int)j, 0x7c), 0, 1, 0);
     }
   }
 }
@@ -55,23 +55,21 @@ void FUN_001bdd80(int tag_index)
 void predicted_resources_precache(void *tag_block)
 {
   int16_t i;
-  int count = *(int *)tag_block;
 
-  for (i = 0; i < count; i++) {
+  for (i = 0; i < *(int *)tag_block; i++) {
     int16_t *element = (int16_t *)tag_block_get_element(tag_block, (int)i, 8);
-    int16_t type = *element;
 
-    if (type == 0) {
-      int16_t bitmap_index = element[1];
-      int tag_datum = *(int *)(element + 2);
-      char *tag_data = (char *)tag_get(0x6269746d, tag_datum);
-      void *bitmaps_block = (void *)(tag_data + 0x60);
-      void *hw_format =
-        tag_block_get_element(bitmaps_block, (int)bitmap_index, 0x30);
-      xbox_texture_cache_get_hardware_format(hw_format, false, true);
-    } else if (type == 1) {
-      int tag_datum = *(int *)(element + 2);
-      FUN_001bdd80(tag_datum);
+    switch (*element) {
+    case 0:
+      xbox_texture_cache_get_hardware_format(
+        tag_block_get_element((char *)tag_get(0x6269746d, *(int *)(element + 2)) + 0x60,
+                              element[1], 0x30),
+        false, true);
+      break;
+
+    case 1:
+      FUN_001bdd80(*(int *)(element + 2));
+      break;
     }
   }
 }
